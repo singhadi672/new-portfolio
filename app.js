8d7eabfcb98815fa18df23841925a8c8cbd968eb
singhadi672 <70839024+singhadi672@users.noreply.github.com>
2021-01-08 22:21:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let slideScene;
+let slides = document.querySelectorAll(".slide");
+let controller = new ScrollMagic.Controller();
+function addAnimationEffects(){
+
+    
+    // slides.forEach(slide=>{
+    //     slideScene = new ScrollMagic.Scene({
+    //         triggerElement:slide,
+    //         triggerHook:0.2
+    //     }).addIndicators().addTo(controller);
+    // })
+    
+}
+
+addAnimationEffects();
